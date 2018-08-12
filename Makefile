4ac4719faebdf37851e56373ebbf54b6e08bd96a
Ken <ken@KendeMacBook-Pro.local>
2018-08-12 11:09:02 +0800

    add new data

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 install:
 	virtualenv --python=python2.7 __
 	pip install --user -r requirements.txt
+	mkdir -r output/
 
-run:
+
+simulate:
 	. __/bin/activate
 	python -m model.simulation
+
+
+
---
+++ model/generate_prediction.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+# generate model
+# generate prediction
+
+def generate_prediction():
+
+
+
+# Model
+
+def generate_model(asset):
+    return
+
+
+def get_model_file_path(asset):
+    return os.path.join()
